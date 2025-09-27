94d77551bd2df03da496232b1435be35cba33fe5
Hoszpodar Patrik <patrikhoszpodar@gmail.com>
2025-09-27 17:23:58 +0200

    React project deleted

---
+++ src/main/java/com/project/sfm2025/security/ApplicationConfig.java
@@ -0,0 +1,25 @@
+/*package com.project.sfm2025.security;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return null;
+            }
+        }
+    }
+}
+*/
