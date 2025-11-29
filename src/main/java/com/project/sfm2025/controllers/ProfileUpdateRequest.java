ddf4aeddc552a6fccc5c24086b538e1791f18432
Hoszpodar Patrik <patrikhoszpodar@gmail.com>
2025-11-29 16:22:31 +0100

    editable user datas

---
+++ src/main/java/com/project/sfm2025/controllers/ProfileUpdateRequest.java
@@ -0,0 +1,15 @@
+package com.project.sfm2025.controllers;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class ProfileUpdateRequest {
+
+    private String firstname;
+    private String lastname;
+    private String password;
+
+}
+
