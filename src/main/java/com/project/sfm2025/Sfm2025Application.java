fab333687ca9f7e3ebfdfcfe10104537fa1eb8dd
kosztafelix <uniteled123@gmail.com>
2025-12-11 08:23:21 +0100

    add Database cleared for presentation, code SFM2025 for coupon

--- src/main/java/com/project/sfm2025/Sfm2025Application.java
+++ src/main/java/com/project/sfm2025/Sfm2025Application.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.*;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -16,37 +17,6 @@ public class Sfm2025Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sfm2025Application.class, args);
-	}
-}
 
-//@RestController
-//class EtelController {
-//
-//    private final String DB_URL = "jdbc:sqlite:src/main/resources/static/data/mydb.sqlite";
-//
-//    @GetMapping("/etelek")
-//    public List<Map<String, Object>> getEtelek() {
-//        List<Map<String, Object>> result = new ArrayList<>();
-//
-//        try (Connection conn = DriverManager.getConnection(DB_URL);
-//             Statement stmt = conn.createStatement();
-//             ResultSet rs = stmt.executeQuery("SELECT * FROM etelek")) {
-//
-//            ResultSetMetaData metaData = rs.getMetaData();
-//            int columnCount = metaData.getColumnCount();
-//
-//            while (rs.next()) {
-//                Map<String, Object> row = new HashMap<>();
-//                for (int i = 1; i <= columnCount; i++) {
-//                    row.put(metaData.getColumnName(i), rs.getObject(i));
-//                }
-//                result.add(row);
-//            }
-//
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
-//
-//        return result;
-//    }
-//}
+    }
+}
